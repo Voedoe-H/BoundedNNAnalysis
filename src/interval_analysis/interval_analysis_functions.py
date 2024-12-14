60d7938cc05a27d121ac0589459f606861439647
Voedoe-H <heermannhagen@gmail.com>
2024-12-14 10:23:59 +0100

    Debug print statements removed

--- src/interval_analysis/interval_analysis_functions.py
+++ src/interval_analysis/interval_analysis_functions.py
@@ -14,10 +14,6 @@ def gemm_layer(input_intervals, weights, biases):
         out = out + biases[i]
         print(f"len of weights: {len(weights[i])}")
         for j in range(len(weights[i])):
-            print(f"J:{j}")
-            print(f"input_intervals[{j}] = {input_intervals[j]} (type: {type(input_intervals[j])})")
-            print(f"weights[{i}][{j}] = {weights[i][j]} (type: {type(weights[i][j])})")
-
             out = out + input_intervals[j] * weights[i][j]
         outputs.append(out)
     return outputs
@@ -39,11 +35,9 @@ def parseONNXModel(model,inputs):
     initializers = {init.name: init for init in graph.initializer}
     constants = {} 
     x = inputs
-    print(x)
     for node in graph.node:
-        print(f"Node Name: {node.name}")
-        print(f"Operation Type: {node.op_type}")
-    
+
+        # Gemm Layer
         if node.op_type == "Gemm":
             print("Linear type case")
             weights_name = node.input[1]
@@ -52,17 +46,28 @@ def parseONNXModel(model,inputs):
             weights = onnx.numpy_helper.to_array(initializers[weights_name])
             biases = onnx.numpy_helper.to_array(initializers[biases_name]) if biases_name else [0] * weights.shape[0]   
 
-            print(x)
             x = gemm_layer(x,weights,biases)
 
             print(f"weights: {weights}")
             print(f"biases: {biases}")
 
+        # Relu Layer
         elif node.op_type == "Relu":
-            print(x)
             x = [inv.relu() for inv in x ]
-            print("Relu type case")
 
+        # Sigmoid Layer
+        elif node.op_type == "Sigmoid":
+            pass
+        
+        # Softplus Layer
+        elif node.op_type == "Softplus":
+            pass
+        
+        # Exponential Layer
+        elif node.op_type == "Exp":
+            pass
+        
+        # Constant Layer
         elif node.op_type == "Constant":
             print("Processing Constant layer")
             for attr in node.attribute:
@@ -70,7 +75,6 @@ def parseONNXModel(model,inputs):
                     # Extract the tensor's value and store it
                     constant_value = onnx.numpy_helper.to_array(attr.t)
                     constants[node.output[0]] = constant_value
-                    print(f"Constant value stored: {constant_value}")
         else:
             raise TypeError(f"Non supported lyer typ: {node.op_type}")
         print("-" * 50)    
